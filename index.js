b6e68d86290dd38ec1169ebabdafea290f8d771a
Erfun <erfan.sahaf@gmail.com>
2017-03-14 02:42:11 +0330

    Update index.js
    Change comments.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var fs          = require("fs");
 var request     = require("request");
-// Get list of domains from list.txt file for whois
+// Get list of domains list from list.txt file for whois
 var domainsList = fs.readFileSync("list.txt", {encoding: "utf-8"}).toString().replace(".com","").replace(".ir","").split("\r\n");
-// The Nic Whois address
+// Nic Whois address
 var baseUrl     = "http://whois.nic.ir/?name=";
 
 // Foreach on domains list
 for(key in domainsList){
-	// Get item one by one
+	// Get items one by one
 	domain = domainsList[key];
-	// Anonymouse function for handle the loop
+	// Anonymouse function for handle the loop iteration
 	(function(address){
 		request(baseUrl + address + ".ir", function (err, response , body){
-			// If request has an error or response will be not Ok (200)
+			// If request has an error or was not Ok ( != 200)
 			if(err || response.statusCode != 200){
 				err = err || response.statusCode;
 				console.log("There is a problem: " + err);
 				return;
 			}
-			// New empty line
+			// New empty console line
 			console.log("");
-			// Domain has an information or not
+			// Check Domain has information or not
 			if(body.includes("person:"))
 				console.log(address + " is Taken");
 			else{
